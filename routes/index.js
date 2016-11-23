4967626abffd5d663c3bc26336266b413d1dd386
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-23 11:00:22 +0100

    Added page routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Welcome to my site'});
+    res.render('index', {title: 'Home'});
 });
 
 module.exports = router;
